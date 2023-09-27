420052a786f9568ae8a893a3c170ed33aa272051
Luke Botros <botrosluke@gmail.com>
2023-09-27 19:16:29 -0400

    Initial Commit

---
+++ Ping Pong/Ping_PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ping_PongApp.swift
+//  Ping Pong
+//
+//  Created by Luke Botros on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Ping_PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
